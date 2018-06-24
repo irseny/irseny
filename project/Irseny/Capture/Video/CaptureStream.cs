b071b55267f6d69509b7cfdb4c21b62ae8afcd6a
xolit <tansa404@hotmail.de>
2018-06-25 01:06:32 +0200

    Irseny: First steps with video capture

---
+++ project/Irseny/Capture/Video/CaptureStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Irseny.Capture.Video {
+	public class CaptureStream {
+
+		object captureSync = new object();
+		Emgu.CV.VideoCapture capture = null;
+
+
+		public CaptureStream() {
+		}
+		public bool Capturing {
+			get {
+				lock (captureSync) {
+					return capture != null;
+				}
+			}
+		}
+		public CaptureSettings Settings {
+			get {
+				lock (captureSync) {
+					return new CaptureSettings();
+				}
+			}
+		}
+
+		public bool Start(CaptureSettings settings) {
+			if (settings == null) throw new ArgumentNullException("settings");
+
+			return false;
+		}
+		public bool Stop() {
+			lock (captureSync) {
+				capture.Stop();
+				capture.Dispose();
+				capture = null;
+			}
+			return true;
+		}
+
+	}
+}
---
+++ project/Irseny/Capture/Video/CaptureSystem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Irseny.Capture.Video {
+	public class CaptureSystem {
+		static object instanceSync;
+		static Thread instanceThread;
+		static volatile CaptureSystem instance;
+
+		object invokeSync = new object();
+		bool running = false;
+		Queue<EventHandler> toInvoke = new Queue<EventHandler>();
+
+
+		static CaptureSystem() {
+			instanceSync = new object();
+			MakeInstance(new CaptureSystem());
+		}
+		public CaptureSystem() {
+
+		}
+
+		public static CaptureSystem Instance {
+			get { return Instance; }
+		}
+
+		public static void MakeInstance(CaptureSystem instance) {
+			if (instance == null) throw new ArgumentNullException("instance");
+			lock (instanceSync) {
+				if (Instance != null) {
+					Instance.SignalStop();
+					instanceThread.Join();
+				}
+				CaptureSystem.instance = instance;
+				instanceThread = new Thread(instance.Run);
+			}
+
+		}
+		private void InvokePending() {
+			Queue<EventHandler> pending;
+			lock (invokeSync) {
+				pending = toInvoke;
+				toInvoke = new Queue<EventHandler>();
+			}
+			foreach (EventHandler handler in pending) {
+				handler(this, new EventArgs());
+			}
+
+		}
+		public void Invoke(EventHandler handler) {
+			lock (invokeSync) {
+				toInvoke.Enqueue(handler);
+			}
+		}
+
+		private void SignalStop() {
+			running = false;
+		}
+		private void Run() {
+			running = true;
+			while (running) {
+				InvokePending();
+				Thread.Sleep(30);
+			}
+		}
+
+
+	}
+}
+
