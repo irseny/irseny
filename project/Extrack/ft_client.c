b7c3bc65ab0c80bc4222b9fdf1f2466e00226591
xolit <tansa404@hotmail.de>
2018-07-11 01:08:09 +0200

    Extrack: Successfully imitated freetrack interface

--- project/Extrack/ft_client.c
+++ project/Extrack/ft_client.c
@@ -1,15 +1,12 @@
 
-
+#include <stdio.h>
 #include <string.h>
 #include <windows.h>
 
 #include "fttypes.h"
 
-#if !defined _WIN64
-#   define FT_EXPORT(t) t __stdcall
-#else
-#   define FT_EXPORT(t) __declspec(dllexport) t
-#endif
+
+#define FT_EXPORT(t) __declspec(dllexport) t
 
 #define dbg_report(...) ((void)0)
 
@@ -21,8 +18,9 @@ static const char* dllProvider = "FreeTrack";
 
 static BOOL impl_create_mapping(void)
 {
-    if (ipc_heap != NULL)
+    if (ipc_heap != NULL && ipc_mutex != NULL) {
         return TRUE;
+	}
 
     hFTMemMap = CreateFileMappingA(INVALID_HANDLE_VALUE,
                                    NULL,
@@ -30,36 +28,80 @@ static BOOL impl_create_mapping(void)
                                    0,
                                    sizeof(FTHeap),
                                    (LPCSTR) FREETRACK_HEAP);
+	
+    if (hFTMemMap == NULL) {
+		printf("failed to create file mapping: %d\n", GetLastError());
+        return FALSE;
+	}
+	//printf("created file mapping\n");
 
-    if (hFTMemMap == NULL)
-        return (ipc_heap = NULL), FALSE;
-
-    ipc_heap = (FTHeap*) MapViewOfFile(hFTMemMap, FILE_MAP_WRITE, 0, 0, sizeof(FTHeap));
+    ipc_heap = (FTHeap*) MapViewOfFile(hFTMemMap, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(FTHeap));
+	if (ipc_heap == NULL) {
+		printf("failed to map file to mem: %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("mapped file to mem\n");
     ipc_mutex = CreateMutexA(NULL, FALSE, FREETRACK_MUTEX);
-
+	if (ipc_mutex == NULL) {
+		printf("failed to create sync: %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("created mutex for synchronization\n");
     return TRUE;
 }
+static BOOL impl_open_mapping(void) {
+	if (ipc_heap != NULL && ipc_mutex != NULL) {
+		return TRUE;
+	}
+	hFTMemMap = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, FREETRACK_HEAP);
+	if (hFTMemMap == NULL) {
+		printf("failed to open file mapping: %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("opened file mapping\n");
+	ipc_heap = (FTHeap*)MapViewOfFile(hFTMemMap, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(FTHeap));
+	if (ipc_heap == NULL) {
+		printf("failed to map file to mem: %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("mapped file to mem\n");
+	ipc_mutex = OpenMutexA(SYNCHRONIZE, FALSE, FREETRACK_MUTEX);
+	if (ipc_mutex == NULL) {
+		printf("failed to open sync: %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("opened mutex for synchronization\n");
+	return TRUE;
+}
+	
 FT_EXPORT(BOOL) FTPutData(FTData* data) {
 	if (impl_create_mapping() == FALSE) {
 		return FALSE;
 	}
 	if (ipc_mutex && WaitForSingleObject(ipc_mutex, 16) == WAIT_OBJECT_0) {
-		memcpy(&ipc_heap->data, data, sizeof(FTData));
+		//printf("moving data from memory to file\n");
+		//memcpy(&ipc_heap->data, data, sizeof(FTData));
+		CopyMemory((PVOID)&ipc_heap->data, data, sizeof(FTData));
 		ipc_heap->data.DataID += 1;
 		ReleaseMutex(ipc_mutex);
+		//printf("finished moving data from memory to file\n");
 	}
 	return TRUE;
 }
 FT_EXPORT(BOOL) FTGetData(FTData* data)
 {
-    if (impl_create_mapping() == FALSE)
+    if (impl_open_mapping() == FALSE)
         return FALSE;
 
     if (ipc_mutex && WaitForSingleObject(ipc_mutex, 16) == WAIT_OBJECT_0) {
-        memcpy(data, &ipc_heap->data, sizeof(FTData));
-        if (ipc_heap->data.DataID > (1 << 29))
+		//printf("moving data from file to memory\n");
+        //memcpy(data, &ipc_heap->data, sizeof(FTData));
+		CopyMemory(data, &ipc_heap->data, sizeof(FTData));
+        if (ipc_heap->data.DataID > (1 << 29)) {
             ipc_heap->data.DataID = 0;
+		}
         ReleaseMutex(ipc_mutex);
+		//printf("finished moving data from file to memory\n");#
     }
     return TRUE;
 }
--- project/Extrack/supply.c
+++ project/Extrack/supply.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <windows.h>
 #include "ft_client.c"
 
-typedef int (__declspec(dllexport) *f_TransmitData)(FTData*);
+typedef BOOL (__declspec(dllimport) *f_TransmitData)(FTData*);
 int main(int argc, char** args) {
 	HINSTANCE h_TransLib = LoadLibrary("libExtrack.dll");
 	if (!h_TransLib) {
@@ -16,7 +16,7 @@ int main(int argc, char** args) {
 	}
 	int signal;
 	FTData data;
-	memset(&data, 0, sizeof(FTData));
+	memset((void*)&data, 0, sizeof(FTData));
 	int iter = 0;
 	do {
 		iter += 1;
@@ -29,4 +29,137 @@ int main(int argc, char** args) {
 	} while (signal != 'q');
 	return 0;
 }
-	
+*/	
+/*#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+
+#define BUF_SIZE 256
+TCHAR szName[]=TEXT("MyFileMappingObject");
+TCHAR szMsg[]=TEXT("Message from first process.");
+
+int _tmain()
+{
+   HANDLE hMapFile;
+   LPCTSTR pBuf;
+
+   hMapFile = CreateFileMappingA(
+                 INVALID_HANDLE_VALUE,    // use paging file
+                 NULL,                    // default security
+                 PAGE_READWRITE,          // read/write access
+                 0,                       // maximum object size (high-order DWORD)
+                 BUF_SIZE,                // maximum object size (low-order DWORD)
+                 szName);                 // name of mapping object
+
+   if (hMapFile == NULL)
+   {
+      _tprintf(TEXT("Could not create file mapping object (%d).\n"),
+             GetLastError());
+      return 1;
+   }
+   pBuf = (LPTSTR) MapViewOfFile(hMapFile,   // handle to map object
+                        FILE_MAP_WRITE, // read/write permission
+                        0,
+                        0,
+                        BUF_SIZE);
+
+   if (pBuf == NULL)
+   {
+      _tprintf(TEXT("Could not map view of file (%d).\n"),
+             GetLastError());
+
+       CloseHandle(hMapFile);
+
+      return 1;
+   }
+
+
+   CopyMemory((PVOID)pBuf, szMsg, (_tcslen(szMsg) * sizeof(TCHAR)));
+    _getch();
+
+   UnmapViewOfFile(pBuf);
+
+   CloseHandle(hMapFile);
+
+   return 0;
+}
+*/
+#include <windows.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <math.h>
+
+typedef struct {
+    uint32_t DataID;
+    int32_t CamWidth;
+    int32_t CamHeight;
+    /* virtual pose */
+    float  Yaw;   /* positive yaw to the left */
+    float  Pitch; /* positive pitch up */
+    float  Roll;  /* positive roll to the left */
+    float  X;
+    float  Y;
+    float  Z;
+    /* raw pose with no smoothing, sensitivity, response curve etc. */
+    float  RawYaw;
+    float  RawPitch;
+    float  RawRoll;
+    float  RawX;
+    float  RawY;
+    float  RawZ;
+    /* raw points, sorted by Y, origin top left corner */
+    float  X1;
+    float  Y1;
+    float  X2;
+    float  Y2;
+    float  X3;
+    float  Y3;
+    float  X4;
+    float  Y4;
+} FTData_t;
+
+int main(int argc, char** args) {
+	HANDLE file = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(FTData_t), "FT_SharedMem");
+	if (file == NULL) {
+		printf("failed to create file mapping\n");
+		return -1;
+	}
+	HANDLE sync = CreateMutexA(NULL, FALSE, "FT_Mutext");
+	if (sync == NULL) {
+		printf("failed to create sync\n");
+		return -1;
+	}
+	FTData_t* map = (FTData_t*)MapViewOfFile(file, FILE_MAP_WRITE, 0, 0, sizeof(FTData_t));
+	if (map == NULL) {
+		printf("failed to map file to memory\n");
+		return -1;
+	}
+	int id = 0;
+	float time = 0;
+	FTData_t data;
+	memset(&data, 0, sizeof(FTData_t));
+	data.CamWidth = 320;
+	data.CamHeight = 240;
+	while (TRUE) {
+		data.DataID = id;
+		data.Yaw = sin(time);
+		data.RawYaw = data.Yaw;
+		
+		printf("Sending packed with id: %d\n", data.DataID);
+		printf("Camera size: %dx%d\n", data.CamWidth, data.CamHeight);
+		printf("Yaw: %f\n", data.Yaw);
+		printf("Pitch: %f\n", data.Pitch);
+		if (WaitForSingleObject(sync, 16) == WAIT_OBJECT_0) {
+			CopyMemory(map, &data, sizeof(FTData_t));
+			ReleaseMutex(sync);
+		}
+		Sleep(16);
+		id += 1;
+		time += 0.01f;
+		
+	} 
+	UnmapViewOfFile(map);
+	CloseHandle(sync);
+	CloseHandle(file);
+}
