b781ccbc3dacda67933adda89cc6297a420831d1
xolit <tansa404@hotmail.de>
2020-07-11 15:11:29 +0200

    Concluded temporary project structure

--- project/Irseny.Main/Program.cs
+++ project/Irseny.Main/Program.cs
@@ -1,8 +1,73 @@
 ﻿using System;
+using System.Net;
+using System.Text;
 namespace Irseny.Main {
 	public static class Program {
 		public static void Main(string[] args) {
 			Console.WriteLine("Hello, World!");
+			var server = new Webface.HttpServer(5643);
+			int requestNo = 0;
+			server.ClientAccepted += (object sender, EventArgs e) => {
+				HttpListenerContext context = (HttpListenerContext)sender;
+				if (context.Request.IsWebSocketRequest) {
+					Console.WriteLine("websocket request!");
+				}
+				string request = context.Request.Url.AbsolutePath;
+				//string name = context.User.Identity.Name;
+				string type = context.Request.ContentType;
+				Console.WriteLine(string.Format("Request {0} of {1}: {2}", requestNo, type, request));
+				requestNo += 1;
+				if (request.StartsWith("/")) {
+					byte[] data = Encoding.UTF8.GetBytes(string.Format(index, requestNo));
+					context.Response.ContentType = "text/html";
+					context.Response.ContentLength64 = data.Length;
+					context.Response.OutputStream.Write(data, 0, data.Length);
+					//context.Response.StatusCode = (int)HttpStatusCode.OK;
+					
+					context.Response.OutputStream.Flush();
+					context.Response.OutputStream.Close();
+
+				}
+			};
+
+			server.Start();
+			System.Threading.Thread.Sleep(30000);
+			server.Stop();
+
 		}
+		const string index = @"
+<!DOCTYPE html>
+<html lang='en'>
+<meat charset='utf-8'/>
+<head>
+
+<title>head</title>
+
+</head>
+<body id='home'>
+
+body of request {0}
+<!--<img src='http://localhost:5643/images/image.png' alt='does not exist' title='an image'>-->
+<script language='javascript'>
+
+var ws = WebSocket('ws://localhost:5643')
+ws.onopen = function(event) {{
+	alert('websocket open');
+}};
+ws.onerror = function(event) {{
+	alert('websocket error: ', event);
+}}
+ws.onclose = function(event) {{
+	alert('websocket closing: ', event);
+}}
+
+</script>
+
+
+<body>
+
+</html>";
+
 	}
+
 }
---
+++ project/Irseny.Main/Webface/HttpServer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace Irseny.Main.Webface {
+	public class HttpServer {
+		Thread listenThread = null;
+		readonly AutoResetEvent stopSignal = new AutoResetEvent(false);
+		public int Port { get; private set; }
+		public bool Started {
+			get {  return listenThread != null; }
+		}
+		public event EventHandler<EventArgs> ClientAccepted; 
+
+
+		public HttpServer(int port) {
+			Port = port;
+		}
+		
+		public void Start() {
+			if (Started) {
+				return;
+			}
+			listenThread = new Thread(Listen);
+			listenThread.Start();
+
+		}
+		public void Stop() {
+			if (!Started) {
+				return;
+			}
+			stopSignal.Set();
+			listenThread.Join();
+			listenThread = null;
+		}
+		private void Listen() {
+			var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8006);
+			listener.Start();
+
+			while (!stopSignal.WaitOne(16)) {
+				if (listener.Pending()) {
+					try { 
+						TcpClient client = listener.AcceptTcpClient();
+						OnClientAccepted(listener, new EventArgs());
+					} catch (SocketException) {
+						// TODO log
+					}
+				}
+			}
+			listener.Stop();
+			//var listener = new HttpListener();
+			//listener.Prefixes.Add(string.Format("http://*:{0}/", Port));
+			//listener.Prefixes.Add(string.Format("ws://*:{0}/", Port));
+			//listener.IgnoreWriteExceptions = true;
+			//listener.Start();
+			//if (!listener.IsListening) {
+			//	return;
+			//}
+			//Task<HttpListenerContext> contextProvider;
+			//try {
+			//	contextProvider = listener.GetContextAsync();
+			//} catch (HttpListenerException e) {
+			//	return;
+			//}
+			//while (!stopSignal.WaitOne(16)) {
+			//	if (contextProvider.IsCompleted) {
+			//		HttpListenerContext context = null;
+			//		try { 
+			//			context = contextProvider.Result;
+			//		} catch (HttpListenerException e) {
+			//			// TODO log
+			//		} finally {
+			//			if (context != null) {
+			//				OnClientAccepted(context, new EventArgs());
+			//			}
+			//		}
+			//		contextProvider = listener.GetContextAsync();
+			//	}
+
+			//}
+
+		}
+		private void OnClientAccepted(object client, EventArgs args) {
+			ClientAccepted(client, args);
+		}
+	}
+}
