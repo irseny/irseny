04bc9bec2a54c32502f59841512f2ab40d4e90e8
irseny <17438554+irseny@users.noreply.github.com>
2021-02-24 21:46:50 +0100

    Irseny.Native: Started test suite - consists of manual tests for freetrack and video capture functionality checking - moved freetrack test to subfolder - added video capture test placeholder that does nothing atm

--- project/Irseny.Native/maketools/build_unix.sh
+++ project/Irseny.Native/maketools/build_unix.sh
@@ -2,7 +2,7 @@
 
 cmake -G "Unix Makefiles" -H. -B../../../build/Irseny.Native \
 -DCMAKE_BUILD_TYPE=Release -DWITH_OPENCV=1 -DWITH_UINPUT=1 \
--DWITH_FREETRACK=1
+-DWITH_FREETRACK=1 -DBUILD_TESTING=1
 
 if [ "$?" -ne "0" ]
 then
