93bf3f8bc07cb4d8ab38f4ee1f587dbb0c6dbefa
irseny <17438554+irseny@users.noreply.github.com>
2021-02-24 21:19:35 +0100

    Irseny.Native: Build detail updates

--- project/Irseny.Native/maketools/build_unix.sh
+++ project/Irseny.Native/maketools/build_unix.sh
@@ -4,5 +4,18 @@ cmake -G "Unix Makefiles" -H. -B../../../build/Irseny.Native \
 -DCMAKE_BUILD_TYPE=Release -DWITH_OPENCV=1 -DWITH_UINPUT=1 \
 -DWITH_FREETRACK=1
 
+if [ "$?" -ne "0" ]
+then
+	exit 1
+fi
+
 make --directory=../../../build/Irseny.Native all
+
+if [ "$?" -ne "0" ]
+then
+	exit 1
+fi
+
 make --directory=../../../build/Irseny.Native install
+
+exit "$?"
